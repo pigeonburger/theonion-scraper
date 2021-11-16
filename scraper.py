e32456cfafeac3d48262639fd7cfd90813846dbb
pigeonburger <70826123+pigeonburger@users.noreply.github.com>
2021-11-17 07:33:51 +1100

    Add Threading support (much faster)

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 import re
+import time
 import requests
+from threading import Thread
 from bs4 import BeautifulSoup
 
 headers = {
@@ -7,6 +9,7 @@ headers = {
 }
 
 def parse_sitemap():
+    global urls
     print('Scanning sitemap.......')
     base = 'https://www.theonion.com'
     r = requests.get(base+'/sitemap', headers=headers)
@@ -15,15 +18,23 @@ def parse_sitemap():
 
     urls = []
     for month in months:
-        month_url = base+month['href']
-        r = requests.get(month_url, headers=headers)
-        soup = BeautifulSoup(r.text, 'html.parser')
-        article_links = [i.find('a')['href'] for i in soup.find_all('h4', class_='js_sitemap-article')]
-        urls.extend(article_links)
+        t = Thread(target=get_sitemap_links, args=(month,))
+        t.daemon = True
+        t.start()
+        time.sleep(0.1)
+        
 
         print(f'Found {len(urls)} articles', end='\r')
     print()
     return urls
+
+def get_sitemap_links(month):
+    base = 'https://www.theonion.com'
+    month_url = base+month['href']
+    r = requests.get(month_url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    article_links = [i.find('a')['href'] for i in soup.find_all('h4', class_='js_sitemap-article')]
+    urls.extend(article_links)
 
 def get_article_content(url):
     r = requests.get(url, headers=headers)
@@ -33,26 +44,28 @@ def get_article_content(url):
 
         content = soup.find('div', class_='js_post-content').find_all('p')
         if content == []:
-            return None
+            h = None
         article_content = [re.sub('<[^<]+?>', '', i.encode_contents().decode('utf-8')) for i in content]
         article_content.insert(0, title)
-        return '\n\n'.join(article_content)
+        h = '\n\n'.join(article_content)
     except:
-        return None
+        h = None
+    
+    if h != None:
+        print(f'Fetched {urls.index(url) + 1} of {len(urls)} articles......', end='\r')
+        with open('theonioncontent.txt', 'a+', encoding='utf-8') as contentfile:
+            contentfile.write('<|startoftext|>'+h+'<|endoftext|>\n')
 
 def main():
+    global urls
     urls = parse_sitemap()
-    total_urls = len(urls)
 
     for url in urls:
-        content = get_article_content(url)
-        if content != None:
-            print(f'Fetched {urls.index(url) + 1} of {len(urls)} articles......', end='\r')
-            with open('theonioncontent.txt', 'a+', encoding='utf-8') as contentfile:
-                contentfile.write('<|startoftext|>'+content+'<|endoftext|>\n')
-        else:
-            total_urls -= 1
-    print(f'\nDone scraping {total_urls} articles.')
+        t = Thread(target=get_article_content, args=(url,))
+        t.daemon = True
+        t.start()
+        time.sleep(0.1)
+    print(f'\nDone scraping articles.')
 
 print('The Onion Article Scraper')
 print('By Pigeonburger <https://github.com/pigeonburger>, 2021')
